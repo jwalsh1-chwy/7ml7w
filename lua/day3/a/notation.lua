eb5da50282d6bbbd87859bece0f1f1c83692e5e7
jwalsh <j@wal.sh>
2020-09-24 19:48:50 -0400

    Calculate notes and duration

--- lua/day3/a/notation.lua
+++ lua/day3/a/notation.lua
@@ -1,20 +1,61 @@
-function parse_note()
+local NOTE_DOWN = 0x90
+local NOTE_UP   = 0x80
+local VELOCITY  = 0x7f
+
+local function note(letter, octave)
+   local notes = {
+      C = 1,
+      Cs = 2,
+      D =  3,
+      Ds = 3,
+      E = 4,
+      F = 5,
+      Fs = 6,
+      G = 7,
+      Gs = 8,
+      A = 9,
+      As = 10,
+      B = 11
+   }
+   local notes_per_octave = 12
+
+   return (octave + 1) * notes_per_octave + notes[letter]
+end
+
+print(note("C", 1))
+
+function dur(value)
+   local tempo = 100
+   local quarter = 60 / tempo
+   local durations = {
+      h  = 2.0,
+      q  = 1.0,
+      ed = 0.75,
+      e  = 0.5,
+      s  = 0.25
+   }
+   return durations[value] * quarter
+end
+
+-- print(duration("ed"))
+
+local function parse_note()
 
 end
 
-function play()
+local function play(note, duration)
 
 end
 
-function part()
+local function part(t)
 
 end
 
-function set_tempo()
+local function set_tempo(bpm)
 
 end
 
-function go()
+local function go()
 
 end
 
