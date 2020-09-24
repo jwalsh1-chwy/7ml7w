37256df76a491b7590edac5dffb08151a44f482e
Jason Walsh <jwalsh1@chewy.com>
2020-09-23 20:43:49 -0400

    Scaffold with `lein new app logical`

--- minikanren/logical/project.clj
+++ minikanren/logical/project.clj
@@ -1,3 +1,10 @@
-(defproject logical "0.0.1-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/core.logic "1.0.0"]])
+(defproject logical "0.1.0-SNAPSHOT"
+  :description "miniKanren"
+  :url "http://wal.sh/"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/core.logic "1.0.0"]]
+  :main ^:skip-aot logical.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
