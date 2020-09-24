ad4cf0409174ada1de7df1dfa5709c5b55f1e30b
jwalsh <j@wal.sh>
2020-09-24 19:27:01 -0400

    Scaffold notation signature

---
+++ lua/day3/a/notation.lua
@@ -0,0 +1,29 @@
+function parse_note()
+
+end
+
+function play()
+
+end
+
+function part()
+
+end
+
+function set_tempo()
+
+end
+
+function go()
+
+end
+
+
+
+return {
+   parse_note = parse_note,
+   play = play,
+   part = part,
+   set_tempo = set_tempo,
+   go = go
+}
