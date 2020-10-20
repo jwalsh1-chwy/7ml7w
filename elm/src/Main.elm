0cfacfe186548e15c7dc4282ef7f0ab400c64ce6
jwalsh <j@wal.sh>
2020-10-20 18:52:44 -0400

    Day 1

---
+++ elm/src/Main.elm
@@ -0,0 +1,21 @@
+module Main exposing (..)
+
+a = [1, 2, 3]
+-- a[2] = 4
+-- a[5] = 6
+
+b = [1, 2] ++ [3, 4]
+
+greet : String -> String
+greet name =
+    "Hello " ++ name ++ "!"
+
+type User = Anonymous | LoggedIn String
+
+x = 0
+
+type Color = Black  | White
+type Piece = Pawn | Knight | Bishop | Rook | Queen | King
+type ChessPiece = CP Color Piece
+
+-- color = case piece of  | CP White _ -> White | CP Black _ -> Black
