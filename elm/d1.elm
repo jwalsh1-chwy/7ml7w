caf3f63a109030a1385f072b64af694e99bf9fe6
jwalsh <j@wal.sh>
2020-10-20 20:37:32 -0400

    Validate Elm 0.17 changes

--- elm/d1.elm
+++ elm/d1.elm
@@ -10,3 +10,13 @@ six = product [1,2,3]
 multiply x y = x * y
 
 fortyeight = (multiply 6) 8
+
+type alias Person =
+    { name : String
+    , age : Int
+    }
+
+jason = { name = "jwalsh", age = 100 }
+
+under50 {age} =
+    age < 50
