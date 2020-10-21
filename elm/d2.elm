734581040d5efcf473107f43bf3b564319dfc0a9
jwalsh <j@wal.sh>
2020-10-20 20:19:19 -0400

    https://elm-lang.org/examples/time

--- elm/d2.elm
+++ elm/d2.elm
@@ -2,9 +2,15 @@ module Main exposing (..)
 
 import Html exposing (..)
 import Browser
+-- import Signal
+
 import Element exposing (..)
 import Html.Events.Extra.Mouse as Mouse
 
+-- https://rosettacode.org/wiki/Mouse_position#Elm
+-- main =
+--     Signal.map show Mouse.position
 
+main : Signal Html
 main =
-    Signal.map show Mouse.position
+    show "Hello World!"
