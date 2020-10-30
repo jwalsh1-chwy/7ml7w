5aa23e4068caf1cdb5bacc7bad7b94e0879c3e75
jwalsh <j@wal.sh>
2020-10-30 17:23:04 -0400

    Show examples for the Pet Health tech

--- elm/d1-explore.elm
+++ elm/d1-explore.elm
@@ -1,4 +1,4 @@
-module DayOne exposing (main)
+module DayOne exposing (main, inc, c, add, twelve)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
@@ -7,7 +7,7 @@ main =
     div [ class "header" ]
         [
          h1 [] [ text "Seven More Languages in Seven Weeks" ],
-         div [] [text c]
+         div [] [text "foo"]
         ]
 
 
@@ -16,6 +16,8 @@ inc = (add 1)
 double n = 2 * n
 
 twelve = 5 |> inc |> double
+tw =  double  <| (5 |> inc)
+-- double(inc(5))
 
 l = List.map double [1, 2, 3]
 
@@ -28,39 +30,39 @@ e = 1 :: [2,3,4]
 f = 1 :: 2 :: 3 :: 4 :: []
 
 
--- case maybeList of
---     Just xs -> xs
---     Nothing -> []
+-- -- case maybeList of
+-- --     Just xs -> xs
+-- --     Nothing -> []
 
--- case xs of
---     [] ->
---         Nothing
---     first :: rest ->
---         Just (first, rest)
+-- -- case xs of
+-- --     [] ->
+-- --         Nothing
+-- --     first :: rest ->
+-- --         Just (first, rest)
 
--- case n of
---     0 -> 1
---     1 -> 1
---     _ -> fib (n-1) + fib (n-2)
+-- -- case n of
+-- --     0 -> 1
+-- --     1 -> 1
+-- --     _ -> fib (n-1) + fib (n-2)
 
 
 
--- create records
+-- -- create records
 origin = { x = 0, y = 0 }
 point = { x = 3, y = 4 }
 
--- access fields
--- origin.x == 0
--- point.x == 3
+-- -- access fields
+-- -- origin.x == 0
+-- -- point.x == 3
 
--- field access function
+-- -- field access function
 -- List.map .x [ origin, point ] == [ 0, 3 ]
 
--- update a field
--- { point | x = 6 } == { x = 6, y = 4 }
+-- -- update a field
+-- -- { point | x = 6 } == { x = 6, y = 4 }
 
--- update many fields
--- { point | x = point.x + 1, y = point.y + 1 }
+-- -- update many fields
+-- -- { point | x = point.x + 1, y = point.y + 1 }
 
 square n =
     n^2
@@ -84,27 +86,27 @@ viewNames2 names =
         |> String.join ", "
 
 
--- let
---     twentyFour =
---         3 * 8
+-- -- let
+-- --     twentyFour =
+-- --         3 * 8
 
---     sixteen =
---         4 ^ 2
--- in
---     twentyFour + sixteen
+-- --     sixteen =
+-- --         4 ^ 2
+-- -- in
+-- --     twentyFour + sixteen
 
 
--- alias for appending lists and two lists
-append sxs sys = sxs ++ sys
+-- -- alias for appending lists and two lists
+-- append sxs sys = sxs ++ sys
 
-xs = [1,2,3]
-ys = [4,5,6]
+-- xs = [1,2,3]
+-- ys = [4,5,6]
 
--- All of the following expressions are equivalent:
--- a1 = append xs ys
-a2 = xs ++ ys
+-- -- All of the following expressions are equivalent:
+-- -- a1 = append xs ys
+-- a2 = xs ++ ys
 
-b2 = (++) xs ys
+-- b2 = (++) xs ys
 
-c1 = (append xs) ys
-c2 = ((++) xs) ys
+-- c1 = (append xs) ys
+-- c2 = ((++) xs) ys
--- factor/examples/fizzbuzz/fizzbuzz.factor
+++ factor/examples/fizzbuzz/fizzbuzz.factor
@@ -1,6 +1,41 @@
-IN: examples.fizzbuzz
-USING: math.functions ;
+USING: kernel io combinators.short-circuit math math.functions
+math.ranges present sequences ;
 
-: fizzbuzz ( n -- string ) 15 mod 0 = "fizzbuzz" ;
+IN: day3.examples.fizzbuzz
 
-30 fizzbuzz
+: fb-trad ( n -- seq )
+    [1,b] [ dup 15 mod 0 =
+            [ drop "FizzBuzz" ]
+            [ dup 3 mod 0 =
+              [ drop "Fizz" ]
+              [ dup 5 mod 0 =
+                [ drop "Buzz" ]
+                [ present ] if
+              ] if
+            ] if
+          ] map ;
+
+! A more ppipeliney solution
+
+: mult? ( x/str n -- ? )
+    over number? [ mod 0 = ] [ 2drop f ] if ;
+
+: when-mult ( x/str n str -- x/str )
+    pick [ mult? ] 2dip ? ;
+
+: fizz ( x/str -- x/str )
+    3 "Fizz" when-mult ;
+
+: buzz ( x/str -- x/str )
+    5 "Buzz" when-mult ;
+
+: fizzbuzz ( x/str -- x/str )
+    15 "FizzBuzz" when-mult ;
+
+: fb-pipeline ( x -- str )
+    fizzbuzz fizz buzz present ;
+
+: fb-with-pipeline ( n -- seq )
+    [1,b] [ fb-pipeline ] map ;
+
+5 fb-with-pipeline
