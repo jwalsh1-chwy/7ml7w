4ffa8003df13d5d09fdac0b9627a0164ac475d24
jwalsh <j@wal.sh>
2020-11-18 09:07:54 -0500

    whicho

--- minikanren/logical/src/logical/geneology.clj
+++ minikanren/logical/src/logical/geneology.clj
@@ -8,3 +8,16 @@
 (defn -main
   [& args]
   (println "Geneology"))
+
+;; todo
+(defn whicho [x s1 s2 r]
+  (conde
+   [(membero x s1)
+    (== r :one)]
+   [(membero x s2)
+    (== r :two)]
+   [(membero x s1)
+    (membero x s2)
+    (== r :both)]))
+
+(run 1 (whicho :a [:a :b :c] [:d :e :f] q))
