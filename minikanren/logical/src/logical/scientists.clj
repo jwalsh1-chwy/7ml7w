b27bde8673f944c0c3134dd32661cc13e990b8ce
jwalsh <j@wal.sh>
2020-11-18 08:52:39 -0500

    Example exercises

---
+++ minikanren/logical/src/logical/scientists.clj
@@ -0,0 +1,10 @@
+(ns logical.scientists
+  (:require [clojure.string :as s]
+            [clojure.set]
+            [clojure.core.logic.pldb]
+            [clojure.core.logic :refer :all]
+            [clojure.core.logic.fd :as fd]))
+
+(defn -main
+  [& args]
+  (println "Scientists"))
