4d883958dbd848d866044f28a995d1c6feacc532
jwalsh <j@wal.sh>
2020-12-03 08:14:12 -0500

    Validate reduce example

--- minikanren/logical/src/logical/petstore.clj
+++ minikanren/logical/src/logical/petstore.clj
@@ -24,11 +24,36 @@
         [[1 0]
          [0 1]
          [-1 0]
-         [0 1]] 
+         [0 1]]
         ]
     (map print-str (map #(opmap (keyword (str %))) ops))
     )
   )
 
-
 (rtr "LLFRRF")
+
+(def position [0 0])
+(def orientation-seq  [[1 0]
+                       [0 1]
+                       [-1 0]
+                       [0 1]])
+(def orientation 3)
+
+
+(vec (map + position (nth orientation-seq orientation)))
+
+(def s "LRLFFFFRRFFFF")
+
+(reduce #(assoc %1 %2 (inc (%1 %2 0)))
+        {}
+        s)
+
+(defn my-reverse [l]
+  (if (empty? l)
+    l
+    (cons (my-reverse (rest l)) (first l))))
+
+
+(my-reverse '(a b c d e f g))
+
+(first '(a b c))
